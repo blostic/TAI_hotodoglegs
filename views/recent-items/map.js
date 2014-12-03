79e6baee7907020aae8fbaaf6f897db2a9656614
mpanuszewska <mpanuszewska@gmail.com>
2014-12-03 03:32:23 +0100

    signing up and logging in possible

--- views/recent-items/map.js
+++ views/recent-items/map.js
@@ -2,10 +2,10 @@ function(doc) {
   if (doc.created_at) {
       var p = doc.profile || {};
       emit(doc.created_at, {
-          message:doc.message,
+          message:doc.comment,
           gravatar_url : p.gravatar_url,
           nickname : p.nickname,
-          name : doc.name
+          name : ""
       });
   }
 };
