5ea840b2a50f7ba54a837da52a7215c08c555ae1
blostic <piotr.skibiak@gmail.com>
2014-12-09 23:03:28 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 HOST="http://127.0.0.1:5984"
 curl -X PUT $HOST/database
 curl -X PUT $HOST/_config/admins/anna -d '"secret"'
-curl -X PUT http://anna:secret@127.0.0.1:5984/dbtest
-curl -H "Content-type: application/json" -d @posts.json -X POST http://anna:secret@127.0.0.1:5984/dbtest/_bulk_docs
-couchapp push . http://anna:secret@127.0.0.1:5984/tai
+curl -X PUT http://anna:secret@127.0.0.1:5984/testdb
+curl -H "Content-type: application/json" -d @posts.json -X POST http://anna:secret@127.0.0.1:5984/testdb/_bulk_docs
